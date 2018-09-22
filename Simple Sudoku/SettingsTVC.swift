9d3b201df32ed129d0fd2a827bb9c49a6a7efc27
theslof <theslof@gmail.com>
2018-09-22 17:31:00 +0200

    Added Settings view with Delete Data button

---
+++ Simple Sudoku/SettingsTVC.swift
@@ -0,0 +1,55 @@
+//
+//  SettingsTVC.swift
+//  Simple Sudoku
+//
+//  Created by Jonas Theslöf on 2018-09-22.
+//  Copyright © 2018 Jonas Theslöf. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTVC: UITableViewController {
+    @IBOutlet weak var deleteCell: UITableViewCell!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.dataSource = self
+        tableView.delegate = self
+
+        tableView.tableFooterView = UIView()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return deleteCell
+    }
+
+    @IBAction func deleteDataBtn(_ sender: UIButton) {
+        let ac = UIAlertController(title: "DELETE", message: "This will delete ALL data permanently. Are you sure?", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Yes", style: .destructive, handler: { _ in
+            let domain = Bundle.main.bundleIdentifier!
+            UserDefaults.standard.removePersistentDomain(forName: domain)
+            UserDefaults.standard.synchronize()
+            self.navigationController?.popToRootViewController(animated: true)
+        }))
+        ac.addAction(UIAlertAction(title: "No", style: .default))
+        self.present(ac, animated: true)
+    }
+    
+}
